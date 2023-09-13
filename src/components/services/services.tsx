96d87682453f1a567962d8fd04f65165b5493950
bruhtesfa-gh <bruhtesfazelealem436@gmail.com>
2023-09-13 12:17:25 +0300

    add last services

--- src/components/services/services.tsx
+++ src/components/services/services.tsx
@@ -14,11 +14,11 @@ const Services = () => {
         setLoading(false);
         let services = [];
         for (let i = 0; i < res.length; i += 2) {
-          console.log("step")
           if (i + 1 < res.length) {
-            console.log("concat")
             const subarray = [res[i], res[i + 1]];
             services.push(subarray);
+          } else if (i < res.length) {
+            services.push([res[i]]);
           }
         }
         setServices(services);
